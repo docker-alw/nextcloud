9df276e101b8a66bccaf0f5f993a9487d7522db4
Andrwe Lord Weber <github@andrwe.org>
2024-06-14 21:27:56 +0200

    fix entrypoint to be version-less (#1)
    Signed-off-by: Andrwe Lord Weber <github@andrwe.org>

--- test.sh
+++ test.sh
@@ -7,3 +7,5 @@ PHP_VERSION=83
 test -e "/usr/sbin/php-fpm${PHP_VERSION}"
 test -L "/var/log/php${PHP_VERSION}/error.log"
 ls -l "/var/log/php${PHP_VERSION}/error.log" | grep "/dev/stderr"
+
+php-fpm -n -v | tr -d '.' | grep " ${PHP_VERSION}"
