21ba8fa924980f7a628deb18cc431cd899badd7d
Andrwe Lord Weber <github@andrwe.org>
2024-06-16 00:57:46 +0200

    improve test-pipeline (#2)
    * improve test-pipeline
    * fix shellcheck issues
    
    ---------
    
    Signed-off-by: Andrwe Lord Weber <github@andrwe.org>

--- test.sh
+++ test.sh
@@ -1,11 +1,16 @@
-#!/bin/sh
+#!/bin/ash
+# shellcheck shell=dash
 
 set -x -e -o pipefail
 
-PHP_VERSION=83
+# shellcheck disable=SC1091
+. /php_version
 
 test -e "/usr/sbin/php-fpm${PHP_VERSION}"
 test -L "/var/log/php${PHP_VERSION}/error.log"
+# shellcheck disable=SC2010
 ls -l "/var/log/php${PHP_VERSION}/error.log" | grep "/dev/stderr"
+test -f "/etc/php${PHP_VERSION}/php-fpm.d/www.conf"
+test -f "/etc/php${PHP_VERSION}/conf.d/opcache.ini"
 
 php-fpm -n -v | tr -d '.' | grep " ${PHP_VERSION}"
