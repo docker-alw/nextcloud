a2847f3f1a8542336b6f731d266e46f641262901
Andrwe Lord Weber <github@andrwe.org>
2024-06-16 00:48:35 +0200

    improve test-pipeline
    Signed-off-by: Andrwe Lord Weber <github@andrwe.org>

--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,11 @@
 # hadolint ignore=DL3007
 FROM ghcr.io/docker-alw/nextcloud-php-modules:latest
 
-COPY test.sh /test.sh
-
 ARG PHP_VERSION=83
+
+RUN echo "PHP_VERSION=${PHP_VERSION}" > /php_version
+
+COPY test.sh /test.sh
 
 # hadolint ignore=DL3018
 RUN apk --no-cache add php${PHP_VERSION}-fpm
--- test.sh
+++ test.sh
@@ -1,11 +1,14 @@
-#!/bin/sh
+#!/bin/ash
+# shellcheck shell=dash
 
 set -x -e -o pipefail
 
-PHP_VERSION=83
+source /php_version
 
 test -e "/usr/sbin/php-fpm${PHP_VERSION}"
 test -L "/var/log/php${PHP_VERSION}/error.log"
 ls -l "/var/log/php${PHP_VERSION}/error.log" | grep "/dev/stderr"
+test -f "/etc/php${PHP_VERSION}/php-fpm.d/www.conf"
+test -f "/etc/php${PHP_VERSION}/conf.d/opcache.ini"
 
 php-fpm -n -v | tr -d '.' | grep " ${PHP_VERSION}"
