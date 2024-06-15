2300c42a886a781a09391910ccdb46ae55cc0cc6
Andrwe Lord Weber <github@andrwe.org>
2024-06-16 00:55:37 +0200

    fix shellcheck issues
    Signed-off-by: Andrwe Lord Weber <github@andrwe.org>

--- test.sh
+++ test.sh
@@ -3,10 +3,12 @@
 
 set -x -e -o pipefail
 
-source /php_version
+# shellcheck disable=SC1091
+. /php_version
 
 test -e "/usr/sbin/php-fpm${PHP_VERSION}"
 test -L "/var/log/php${PHP_VERSION}/error.log"
+# shellcheck disable=SC2010
 ls -l "/var/log/php${PHP_VERSION}/error.log" | grep "/dev/stderr"
 test -f "/etc/php${PHP_VERSION}/php-fpm.d/www.conf"
 test -f "/etc/php${PHP_VERSION}/conf.d/opcache.ini"
